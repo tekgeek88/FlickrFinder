9c73f6fe17567086915b3a53b066e7039ebe4be2
tekgeek88 <tekgeek88@yahoo.com>
2015-12-18 06:21:54 -0800

    Added AFNetworking Framework

---
+++ Flickr Finder/bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  Flickr Finder
+//
+//  Created by Carl Argabright on 12/17/15.
+//  Copyright © 2015 Carl Argabright. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+#import "AFNetworking.h"
+
+#endif /* bridge_h */
+
